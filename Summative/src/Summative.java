be3bd60d6fed12fc950563f10053238d3d2cb104
Jeffrey Zhao <142653175+jeffreyzhao32@users.noreply.github.com>
2024-01-15 15:05:13 -0500

    Update Summative.java

--- Summative/src/Summative.java
+++ Summative/src/Summative.java
@@ -3,7 +3,7 @@
  * Program: matching memory game teaching students how to solve one variable algebraic equations
  * Description: First tab is an introduction of the program to the user, second teaches students how to 
  * solve algebraic equations with an example. It also provides user with instruction on how to play the
- * game. In the game, there are 12 panels with pictures, users are allowed to select on 2 panels at once. If 
+ * game. In the game, there are 12 panels with pictures, users are allowed to select on 2 panels at once. 
  */
 
 import javax.swing.*;
